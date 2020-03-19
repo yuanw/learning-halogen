9b0d20e1ee7c42d9fb509b123e3278c0de339241
Yuan Wang <me@yuanwang.ca>
2020-03-18 22:21:45 -0600

    add random number example

--- src/Main.purs
+++ src/Main.purs
@@ -2,50 +2,63 @@ module Main where
 
 
 -- Imports for lesson
-import Halogen.HTML as HH
 
--- Imports for scaffolding
 import Prelude
 
-import Data.Const (Const)
+import Data.Maybe (Maybe(..), maybe)
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_)
+import Effect.Class (class MonadEffect)
+import Effect.Random (randomInt)
 import Halogen as H
-import Halogen.Aff (awaitBody)
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
-import Effect.Console (log)
-
--- | HTML written in Purescript via Halogen's HTML DSL
--- | that is always rendered the same and does not include any event handling.
-type StaticHTML = H.ComponentHTML Unit () Aff
 
 
--- | Shows how to use Halogen VDOM DSL to render HTML without properties or CSS
-staticHtml :: StaticHTML
-staticHtml =
-  HH.div_
-    -- The 'div' tag takes an Array of children
-    [ HH.div_
-      [ HH.span_
-        -- as does the `span` tag
-        [ HH.text "This is text in a span!" ]
-      ]
-    , HH.button_
-      [ HH.text "You can click me, but I don't do anything." ]
-    ]
+type State = Maybe Int
 
--- | Wraps Halogen types cleanly, so that one gets very clear compiler errors
-staticComponent :: StaticHTML
-                -> H.Component HH.HTML (Const Unit) Unit Void Aff
-staticComponent renderHtml =
+data Action = Regenerate
+
+component :: forall q i o m. MonadEffect m =>  H.Component HH.HTML q i o m
+component =
   H.mkComponent
-    { initialState: const unit
-    , render: \_ -> renderHtml
-    , eval: H.mkEval H.defaultEval
-    }
-    
+     { initialState
+     , render
+     , eval: H.mkEval $ H.defaultEval {handleAction = handleAction}
+     }
+
+
+initialState :: forall i. i -> State
+initialState = const Nothing
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render state =
+  let
+    value = maybe "No number generated yet" show state
+  in
+    HH.div_ $
+      [ HH.h1_ [ HH.text "Random number" ]
+      , HH.p_ [ HH.text ("Current value: " <> value) ]
+      , HH.button
+          [ HE.onClick \_ -> Just Regenerate ]
+          [ HH.text "Generate new number" ]
+      ]
+
+
+
+
+handleAction :: forall o m. MonadEffect m => Action -> H.HalogenM State Action () o m Unit
+handleAction = case _ of
+  Regenerate -> do
+    newNumber <- H.liftEffect $ randomInt 0 100
+    H.put (Just newNumber)
+
+
+
 main :: Effect Unit
-main = launchAff_ do
+main = HA.runHalogenAff do
   --log "🍝"
-  body <- awaitBody
-  runUI (staticComponent staticHtml) unit body
+  body <- HA.awaitBody
+  --runUI (staticComponent staticHtml) unit body
+  runUI component unit body
